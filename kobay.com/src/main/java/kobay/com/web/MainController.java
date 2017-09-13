d93b777b7d6e4504cb257b138b6a16fd264ff7d2
ezenProject <pialissue@gmail.com>
2017-09-13 17:17:45 +0900

    전자정부 웹 템플릿기반 기본프레임워크(기본템플릿수정)

--- kobay.com/src/main/java/kobay/com/web/MainController.java
+++ kobay.com/src/main/java/kobay/com/web/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import kobay.com.service.TestService;
 import kobay.com.service.TestVO;
 
