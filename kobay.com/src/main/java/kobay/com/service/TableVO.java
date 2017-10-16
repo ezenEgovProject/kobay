d59cd6807b7760a5ee61ef45c6a0212c27f9551f
김종연 <rla210218@naver.com>
2017-10-16 20:36:18 +0900

    v1.0.3 summernote 적용했는데 저장시 object:object error 뜸 ㅜㅜ

--- kobay.com/src/main/java/kobay/com/service/TableVO.java
+++ kobay.com/src/main/java/kobay/com/service/TableVO.java
@@ -2,8 +2,8 @@ package kobay.com.service;
 
 public class TableVO {
 	
-	private int auction_unq;
-	private int member_unq;
+	private int auctionunq;
+	private int memberunq;
 	private int seqNumber;
 	
 	public int getSeqNumber() {
@@ -12,17 +12,17 @@ public class TableVO {
 	public void setSeqNumber(int seqNumber) {
 		this.seqNumber = seqNumber;
 	}
-	public int getAuction_unq() {
-		return auction_unq;
+	public int getAuctionunq() {
+		return auctionunq;
 	}
-	public void setAuction_unq(int auction_unq) {
-		this.auction_unq = auction_unq;
+	public void setAuctionunq(int auction_unq) {
+		this.auctionunq = auction_unq;
 	}
-	public int getMember_unq() {
-		return member_unq;
+	public int getMemberunq() {
+		return memberunq;
 	}
-	public void setMember_unq(int member_unq) {
-		this.member_unq = member_unq;
+	public void setMemberunq(int member_unq) {
+		this.memberunq = member_unq;
 	}
 	
 }
